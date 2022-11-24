a6db6dafd5358a088ff61e30f967e910f4bc6bc5
sukhdev-bajiya <bajiya4442@gmail.com>
2022-11-24 13:03:20 +0530

    admin part

--- server/middleware/studentauth.js
+++ server/middleware/studentauth.js
@@ -3,20 +3,38 @@ import devtechUserModel from "../model/user.model.js";
 const StudentAuthRouter = express.Router();
 
 StudentAuthRouter.get("/getalluserlist", async (req, res) => {
-  let user = req.header("Authorization")
-  let userType = req.header("userType")
+  let user = req.header("Authorization");
+  let usertype = req.header("userType");
   try {
     let userAuth = await devtechUserModel.findById(user);
-    if (userAuth && (userAuth.role === "admin" || userAuth.role === "teacher")) {
-      let devtechUser = await devtechUserModel.findAll({ role: userType });
-      return res.status(201).send({ success: true, error: false, message: "", user: devtechUser[0] });
+    if (
+      usertype !== "admin" &&
+      userAuth &&
+      (userAuth.role === "admin" || userAuth.role === "teacher")
+    ) {
+      let devtechUser = await devtechUserModel.find(
+        { role: usertype },
+        {
+          securityQuestion1: 0,
+          securityAnswer1: 0,
+          securityQuestion2: 0,
+          securityAnswer2: 0,
+        }
+      );
+      return res
+        .status(201)
+        .send({ success: true, error: false, message: "", user: devtechUser });
     } else {
-      return res.status(201).send({ success: false, error: true, message: "Wrong Credentials", user: {} });
+      return res.status(201).send({
+        success: false,
+        error: true,
+        message: "Wrong Credentials",
+        user: {},
+      });
     }
   } catch (error) {
     return res.status(500).send(error.message);
   }
 });
 
-
-export default StudentAuthRouter
+export default StudentAuthRouter;
